2ccce0c5431c4d1c203a19dabde697114ce40651
xtxyTge2 <f1AZnn@protonmail.com>
2023-06-30 20:38:01 +0200

    Completed Exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,9 +10,17 @@ const Content = (props) => {
   console.log(props)
   return (
     <>
-      <p>{props.part1} {props.exercise1}</p>
-      <p>{props.part2} {props.exercise2}</p>
-      <p>{props.part3} {props.exercise3}</p>
+      <Part part={props.part1} exercise={props.exercise1}></Part>
+      <Part part={props.part2} exercise={props.exercise2}></Part>
+      <Part part={props.part3} exercise={props.exercise3}></Part>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercise}</p>
     </>
   )
 }
